b593ecaaf9056b8d2a58b6cd7e0f0ccc658534fc
Esteban Viera <esteban.viera.arroba@gmail.com>
2022-09-14 01:35:51 -0300

    Fix ipfs

--- src/shared/services/ipfs.service.ts
+++ src/shared/services/ipfs.service.ts
@@ -9,6 +9,6 @@ export class IPFSClientService {
   public async upload(base64_string: string): Promise<string> {
     const data = fromString(base64_string, 'base64')
     const { cid } = await this.client.add(data)
-    return `https://cloudflare-ipfs.com/ipfs/${cid}}`
+    return `https://cloudflare-ipfs.com/ipfs/${cid}`
   }
 }
