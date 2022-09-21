1e56d68ee0e2e8ab2ed6cba6c3e141b7a5bfdff0
Esteban Viera <esteban.viera.arroba@gmail.com>
2022-09-21 17:58:37 -0300

    Module providers

--- src/modules/team/team.module.ts
+++ src/modules/team/team.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ApiServicesModule } from '../../api-services/api-services.module';
+import { TeamSubscriber } from '../../entity-subscribers/team-subscriber';
 import { UserModule } from '../../modules/user/user.module';
 import { IPFSClientService } from '../../shared/services/ipfs.service';
 import { TeamController } from './team.controller';
@@ -11,7 +12,7 @@ import { TeamService } from './team.service';
 @Module({
   imports: [TypeOrmModule.forFeature([TeamEntity]), UserModule, ApiServicesModule],
   controllers: [TeamController],
-  exports: [TeamService],
-  providers: [TeamService],
+  exports: [TeamService, TeamSubscriber],
+  providers: [TeamService, TeamSubscriber],
 })
 export class TeamModule { }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ApiServicesModule } from '../../api-services/api-services.module';
+import { UserSubscriber } from '../../entity-subscribers/user-subscriber';
 import { UserTeamSettingsEntity } from './user-team-settings.entity';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
@@ -10,7 +11,7 @@ import { UserService } from './user.service';
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, UserTeamSettingsEntity]), ApiServicesModule],
   controllers: [UserController],
-  exports: [UserService],
-  providers: [UserService],
+  exports: [UserService, UserSubscriber],
+  providers: [UserService, UserSubscriber],
 })
 export class UserModule { }
