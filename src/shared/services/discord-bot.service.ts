525bf94b68568f3e81f22291d6d48b68fed03c84
Esteban Viera <esteban.viera.arroba@gmail.com>
2022-09-23 14:29:11 -0300

    Mejorar application message

--- src/shared/services/discord-bot.service.ts
+++ src/shared/services/discord-bot.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CategoryChannel, ChannelType, Client, Guild, TextChannel } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CategoryChannel, ChannelType, Client, EmbedType, Guild, TextChannel } from 'discord.js';
 import { UserEntity } from '../../modules/user/user.entity';
 
 import { TeamEntity } from '../../modules/team/team.entity';
@@ -65,20 +65,46 @@ export class DiscordBotService {
 
     const channel = category.children.cache.find(channel => channel.type === ChannelType.GuildText) as TextChannel
 
-    const row = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId(`${team.id}|${applicant.address}|deny`)
-          .setLabel('Deny!')
-          .setStyle(ButtonStyle.Secondary),
-      )
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId(`${team.id}|${applicant.address}|accept`)
-          .setLabel('Accept!')
-          .setStyle(ButtonStyle.Primary),
-      );
+    await channel.send({
+      "content": "",
+      "tts": false,
+      "components": [
+        {
+          "type": 1,
+          "components": [
+            {
+              "style": 2,
+              "label": `Deny`,
+              "custom_id": `${team.id}|${applicant.address}|deny`,
+              "disabled": false,
+              "type": 2
+            },
+            {
+              "style": 1,
+              "label": `Accept`,
+              "custom_id": `${team.id}|${applicant.address}|accept`,
+              "disabled": false,
+              "type": 2
+            }
+          ]
+        }
+      ],
+      "embeds": [
+        {
+          "type": EmbedType.Rich,
+          "title": `New applicant`,
+          "description": "",
+          "color": 0x5100ff,
+          "fields": [
+            {
+              "name": `${applicant.address}`,
+              "value": `${applicant.name ? applicant.name + "\n" : ''}${applicant.socialLinks?.length ? applicant.socialLinks.map(a => a.name + ": " + a.link).join("\n") : ''}`,
+            }
+          ]
+        }
+      ]
+    })
 
-    await channel.send({ content: `New applicant: ${applicant.address}!`, components: [(row as any)] });
+    await channel.send({ content: `New applicant: ${applicant.address}`, components: [(row as any)] });
   }
 }
