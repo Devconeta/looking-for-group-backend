3a96f6ac1b59a3fb46a57166fc54cfde32b1597e
AxelGes <axelges9@gmail.com>
2022-09-25 15:04:46 -0300

    fix required

--- src/modules/team/dtos/TeamCreateDto.ts
+++ src/modules/team/dtos/TeamCreateDto.ts
@@ -8,60 +8,61 @@ import { TeamTags, UserRole } from '../../../constants';
 import { Trim } from '../../../decorators/transform.decorators';
 
 export class TeamCreateDto {
-  @ApiPropertyOptional()
-  @IsOptional()
-  @IsString()
-  readonly address: string;
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    readonly address: string;
 
-  @ApiPropertyOptional()
-  @IsString()
-  @IsNotEmpty()
-  readonly name: string;
+    @ApiPropertyOptional()
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
 
-  @ApiPropertyOptional()
-  @IsString()
-  @IsOptional()
-  @Trim()
-  readonly idea: string;
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @Trim()
+    readonly idea: string;
 
-  @ApiPropertyOptional()
-  @IsString()
-  @IsOptional()
-  @Trim()
-  readonly avatar: string;
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @Trim()
+    readonly avatar: string;
 
-  @ApiPropertyOptional()
-  @IsNumber()
-  @Max(8)
-  @Min(1)
-  readonly maxMembers: number;
+    @ApiPropertyOptional()
+    @IsNumber()
+    @Max(8)
+    @Min(1)
+    @IsOptional()
+    readonly maxMembers: number;
 
-  @ApiPropertyOptional()
-  @Column()
-  @IsBoolean()
-  @IsOptional()
-  readonly isPublic: boolean;
+    @ApiPropertyOptional()
+    @Column()
+    @IsBoolean()
+    @IsOptional()
+    readonly isPublic: boolean;
 
-  @ApiPropertyOptional()
-  @IsBoolean()
-  @IsOptional()
-  readonly isContractDeployed: boolean;
+    @ApiPropertyOptional()
+    @IsBoolean()
+    @IsOptional()
+    readonly isContractDeployed: boolean;
 
-  @ApiPropertyOptional()
-  @IsString()
-  @IsOptional()
-  @Trim()
-  readonly slogan: string;
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @Trim()
+    readonly slogan: string;
 
-  @ApiPropertyOptional({ enum: TeamTags, isArray: true, type: Array })
-  @IsOptional()
-  @IsArray()
-  readonly tags?: TeamTags[];
+    @ApiPropertyOptional({ enum: TeamTags, isArray: true, type: Array })
+    @IsOptional()
+    @IsArray()
+    readonly tags?: TeamTags[];
 
-  @ApiPropertyOptional({ enum: UserRole, isArray: true, type: Array })
-  @IsOptional()
-  @IsArray()
-  readonly lookingFor?: UserRole[];
+    @ApiPropertyOptional({ enum: UserRole, isArray: true, type: Array })
+    @IsOptional()
+    @IsArray()
+    readonly lookingFor?: UserRole[];
 
-  members: UserEntity[];
+    members: UserEntity[];
 }
